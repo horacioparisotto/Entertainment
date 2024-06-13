e94952979f735c9283cf9bbfe3643a74f203c746
Hoacio Parisotto <parisottohoracio@gmail.com>
2024-06-13 11:20:24 -0300

    SignIn

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -3,39 +3,38 @@ import "./SignupScreen.css";
 import { auth } from "../firebase";
 
 function SignupScreen() {
-    const emailRef = useRef(null);
-    const passwordRef = useRef(null);
-  
-    const register = (e) => {
-      e.preventDefault();
-  
-      auth
-        .createUserWithEmailAndPassword(
-          emailRef.current.value,
-          passwordRef.current.value
-        )
-        .then((authUser) => {
-          console.log(authUser);
-        })
-        .catch((error) => {
-          alert(error.message);
-        });
-    };
-  
-    const signIn = (e) => {
-      e.preventDefault();
-  
-      auth
-        .signInWithEmailAndPassword(
-          emailRef.current.value,
-          passwordRef.current.value
-        )
-        .then((authUser) => {
-          console.log(authUser);
-        })
-        .catch((error) => alert(error.message));
-    };
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
 
+  const register = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <div className="signupScreen">
@@ -43,7 +42,7 @@ function SignupScreen() {
         <h1>Sign In</h1>
         <input ref={emailRef} placeholder="Email" type="email" />
         <input ref={passwordRef} placeholder="Password" type="password" />
-        <button type="submit" onClick={signIn}>
+        <button type="submit" onClick={register}>
           Sign In
         </button>
 
